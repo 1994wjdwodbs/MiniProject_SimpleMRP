74d49c43f761186230865ab2bf774a9d5068ac61
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-06-21 14:36:55 +0900

    test

---
+++ MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
@@ -0,0 +1,100 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MRPApp.View.Setting
+{
+    /// <summary>
+    /// MyAccount.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingList : Page
+    {
+        public SettingList()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {/*
+                // Store 테이블 데이터 읽어와야 함
+                List<Model.Store> stores = new List<Model.Store>();
+                List<Model.StockStore> stockStores = new List<Model.StockStore>();
+                stores = Logic.DataAccess.GetStores(); // 수영1 ...
+
+                // TODO : stores 데이터를 stockStores로 복사
+                foreach (Model.Store item in stores)
+                {
+                    var store = new Model.StockStore()
+                    {
+                        StoreID = item.StoreID,
+                        StoreName = item.StoreName,
+                        StoreLocation = item.StoreLocation,
+                        ItemStatus = item.ItemStatus,
+                        TagID = item.TagID,
+                        BarcodeID = item.BarcodeID,
+                        StockQuantity = 0
+                    };
+                    // 
+                    store.StockQuantity = Logic.DataAccess.GetStocks().Where(t => t.StoreID.Equals(store.StoreID)).Count();
+
+                    stockStores.Add(store);
+                }
+
+                this.DataContext = stockStores;*/
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 StoreList Loaded : {ex}");
+                throw ex;
+            }
+        }
+
+        private void BtnEditUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //NavigationService.Navigate(new EditUser());
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnEditUser_Click : {ex}");
+                throw ex;
+            }
+        }
+
+        private void BtnAddStore_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // NavigationService.Navigate(new AddStore());
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnAddStore_Click : {ex}");
+                throw ex;
+            }
+        }
+
+        private void BtnEditStore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExportExcel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
